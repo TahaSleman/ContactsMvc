64d3d68dbaadaaf47283147d15b23df803549f9b
Taha <tahasleman1992@gmail.com>
2019-08-07 03:43:28 -0700

    Full Project

--- TelerikMvcApp2/App_Start/RouteConfig.cs
+++ TelerikMvcApp2/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TelerikMvcApp2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Contacts", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
